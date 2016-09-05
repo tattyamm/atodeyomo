15f5137b6baa561b813697a1a053301e50dbe38c
tattyamm <entatsu.appli+github@gmail.com>
2016-09-05 16:32:47 +0000

    add source getter in js

--- public/js/core.js
+++ public/js/core.js
@@ -4,14 +4,18 @@
 	var formeObj = document.createElement('form');
 	var urlObj = document.createElement('input');
 	var titleObj = document.createElement('input');
+	var sourceObj = document.createElement('input');
 	urlObj.name = "url";
 	urlObj.value = location.href;
 	titleObj.name = "webdatum";
 	titleObj.value = document.title;
+	sourceObj.name = "source";
+	sourceObj.value = document.lastChild.innerHTML;
 
 	// form
 	formeObj.appendChild(urlObj);
 	formeObj.appendChild(titleObj);
+	formeObj.appendChild(sourceObj);
 	formeObj.style.display = 'none';
 	try{
 		document.body.appendChild(formeObj);
@@ -20,12 +24,12 @@
 	}
 
 	// submit
-	var chr = document.charset;
+	var pageCharset = document.charset;
 	formeObj.action = "https://atodeyomo.herokuapp.com/api/webdata";
 	formeObj.method = "POST";
 	formeObj.acceptCharset = "utf-8";
 	document.charset = "utf-8";
 	formeObj.submit();
-	document.charset = chr;
+	document.charset = pageCharset;
 
 })();
