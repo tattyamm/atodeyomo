ed761e5ecd8fe6ecfe0bbc8481ea58ae2ee5a748
tattyamm <entatsu.appli+github@gmail.com>
2016-09-24 15:38:47 +0000

    add mail temp

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'jbuilder', '~> 2.5'
 
 # 追加
 gem 'figaro'
+gem 'sendgrid-ruby'
 
 group :production do
   gem 'pg'
--- app/controllers/webdata_controller.rb
+++ app/controllers/webdata_controller.rb
@@ -1,3 +1,5 @@
+include SendGrid
+
 class WebdataController < ApplicationController
   before_action :set_webdatum, only: [:show, :edit, :update, :destroy]
 
@@ -70,10 +72,28 @@ class WebdataController < ApplicationController
     webdatum = Webdatum.new({url: params[:url], title:params[:title], webdata:webdatum_formatted})
     p webdatum
     webdatum.save
+    send_mail
 
     redirect_to(params[:url])
   end
 
+  #TODO 外部から色々指定する
+  # メール送信
+  def send_mail
+    from = Email.new(email: ENV['MY_MAIL_FROM'])
+    subject = 'test mail!'
+    to = Email.new(email: ENV['MY_MAIL_TO'])
+#    content = Content.new(type: 'text/plain', value: 'Hello, Email!')
+#    content = Content.new(type: 'text/html', value: '<html><head></head><body>Hello, <br><b>Email!</b></body></html>')
+    content = Content.new(type: 'text/html', value: 'Hello, <br><b>Email!</b>')
+    mail = Mail.new(from, subject, to, content)
+
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
+    puts response.status_code
+    puts response.body
+    puts response.headers
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
