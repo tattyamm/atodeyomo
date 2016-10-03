ab87f3b5ab905309ad122ea404d64397625eb0bb
tattyamm <entatsu.appli+github@gmail.com>
2016-10-03 17:10:37 +0000

    fix mail class conflict

--- app/controllers/concerns/common.rb
+++ app/controllers/concerns/common.rb
@@ -2,10 +2,10 @@ module Common
   extend ActiveSupport::Concern
 
   def send_mail(from_mail, from_to, subject, body)
-    from = Email.new(email: from_mail)
-    to = Email.new(email: from_to)
+    from = SendGrid::Email.new(email: from_mail)
+    to = SendGrid::Email.new(email: from_to)
     content = Content.new(type: 'text/html', value: body)
-    mail = Mail.new(from, subject, to, content)
+    mail = SendGrid::Mail.new(from, subject, to, content)
 
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
     response = sg.client.mail._('send').post(request_body: mail.to_json)
